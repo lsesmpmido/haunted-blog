eb241ff925ef7a5df3b216ef0d496d4320ba541b
r.kawano <r.kawano@genbasupport.com>
2025-02-17 13:09:25 +0900

    他の人のブログ記事を編集できる脆弱性を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :can_edit_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -49,5 +50,11 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+  end
+
+  def can_edit_blog
+    return if @blog.user.id == current_user.id
+
+    raise ActiveRecord::RecordNotFound
   end
 end
