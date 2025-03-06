080e1b79801e85c51a91707a82a1ead618b0d440
r.kawano <r.kawano@genbasupport.com>
2025-03-06 09:06:03 +0900

    `set_blog()`を削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,6 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show]
   before_action :can_edit_blog, only: %i[edit update destroy]
 
   def index
@@ -45,10 +44,6 @@ class BlogsController < ApplicationController
   end
 
   private
-
-  def set_blog
-    @blog = Blog.find(params[:id])
-  end
 
   def blog_params
     permitted_params = %i[title content secret]
