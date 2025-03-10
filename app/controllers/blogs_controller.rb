a04f00a2c6c5af9b71f995409e594c395509d5f4
r.kawano <r.kawano@genbasupport.com>
2025-03-10 09:45:56 +0900

    メソッド名を修正（`set_blog_for_edit()` → `set_blog_for_current_user()`）

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog_for_edit, only: %i[edit update destroy]
+  before_action :set_blog_for_current_user, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -51,7 +51,7 @@ class BlogsController < ApplicationController
     params.require(:blog).permit(*permitted_params)
   end
 
-  def set_blog_for_edit
+  def set_blog_for_current_user
     @blog = current_user.blogs.find(params[:id])
   end
 end
